ddfb7927200a541c3e1cce737257c7e06a80ef04
Rodrigo Mendoza <rmendoza@mendozablanco.com.mx>
2016-12-08 17:12:22 +0100

    Added simple NN mnist data set

--- intro_01.py
+++ intro_01.py
@@ -1,8 +1,52 @@
 import tensorflow as tf
 
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 
 
+x = tf.placeholder(tf.float32, [None, 784])
+
+W = tf.Variable(tf.random_uniform([784, 200], -1, 1))
+
+
+# W = tf.Variable(tf.zeros([784, 200]))
+b = tf.Variable(tf.zeros([200]))
+
+W2 = tf.Variable(tf.zeros([200, 10]))
+b2 = tf.Variable(tf.zeros([10]))
+
+
+hidden1 = tf.nn.relu(tf.matmul(x, W) + b)
+
+
+y = tf.nn.softmax(tf.matmul(hidden1, W2) + b2)
+
+
+
+# y = tf.nn.softmax(tf.matmul(x, W) + b)
+y_ = tf.placeholder(tf.float32, [None, 10])
+
+
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+
+train_step = tf.train.GradientDescentOptimizer(0.03).minimize(cross_entropy)
+
+
+
+
+init = tf.initialize_all_variables()
+
+sess = tf.Session()
+sess.run(init)
+
+for i in range(1000):
+  batch_xs, batch_ys = mnist.train.next_batch(100)
+  sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+  correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+  accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+  print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
 
 
 
